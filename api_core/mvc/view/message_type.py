9c5b37cf5e51ba0736529392814db1d37651da37
DevPAMGit <49749709+seedbaobab@users.noreply.github.com>
2023-06-20 22:53:51 +0200

    Ajout du type de message.

---
+++ api_core/mvc/view/message_type.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class MessageType(Enum):
+    """
+    Enumeration for available message types.
+    """
+    DEBUG: str = "DEBUG",
+    INFO: str = "INFO",
+    SUB_INFO: str = "  INFO    ->",
+    WARN: str = "WARNING",
+    ERROR: str = "ERROR",
+    INPUT: str = "INPUT",
+    SUCCESS: str = "SUCCESS",
+    SUB_SUCCESS: str = "  SUCCESS ->",
+    MANUAL: str = "MANUAL",
+    START: str = "START",
+    END: str = "END"
+
+    @staticmethod
+    def maximum_space() -> int:
+        """
+        Get the maximum size (in number of characters) of the largest enumeration.
+        :return: The maximum size (in number of characters) of the largest enumeration.
+        """
+        value: int = 0
+        for enum_value in MessageType:
+            len_enum_value: int = len(enum_value.value)
+            if len_enum_value.__gt__(value):
+                value = len_enum_value
+        return value
