023b1de1f338908ef3aa980946b1b464bb477447
DevPAMGit <49749709+seedbaobab@users.noreply.github.com>
2023-06-21 02:05:12 +0200

    Création de la classe 'StringHelper'.

---
+++ api_core/helper/string_helper.py
@@ -0,0 +1,31 @@
+class StringHelper:
+    """
+    Helper class for String.
+    """
+
+    @staticmethod
+    def is_empty(value: Optional[str]) -> bool:
+        """
+        Method checking if the parameter value is null or empty.
+        :param value: The value to test.
+        :return: True if the value is None or empty.
+        """
+        return True if value is None or value.strip().__eq__("") else False
+
+    @staticmethod
+    def has_space(value: Optional[str]) -> bool:
+        """
+        Method that checks if the parameter value contains a space.
+        :param value: The value to test.
+        :return: True if the parameter value contains space is None or empty.
+        """
+        return False if value is None or not (' ' in value.strip()) else True
+
+    @staticmethod
+    def is_alphabetic(value: str) -> bool:
+        return any(c.isalpha() for c in value)
+
+    @staticmethod
+    def to_camel_case(value: str) -> str:
+        return sub(r"(_|-)+", " ", value).title().replace(" ", "")
+
