deea206da3e53ce90edae4c12d530b27352f6b4d
DevPAMGit <49749709+seedbaobab@users.noreply.github.com>
2023-06-21 02:05:40 +0200

    Création de la classe 'ConstantHelper'.

---
+++ api_core/helper/constant_helper.py
@@ -0,0 +1,9 @@
+class ConstantHelper:
+    """
+    Class that contains all constant of the project.
+    """
+
+    SCREEN_SIZE: int = 103
+    """
+    The width of the console view (in characters).
+    """
