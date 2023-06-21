2b52ea7fb7624c239a9633a0286a28b72fd9b766
DevPAMGit <49749709+seedbaobab@users.noreply.github.com>
2023-06-21 02:04:29 +0200

    Création de la classe 'ControllerService'.

---
+++ api_core/mvc/service/model/controller_service.py
@@ -0,0 +1,36 @@
+from api_core.mvc.controller.controller import Controller
+
+
+class ControllerService:
+    """
+    Service class to manage API controllers.
+    """
+
+    def __init__(self):
+        """
+        Initialize a new instance of 'ControllerService' class.
+        """
+        self.__CONTROLLERS: dict[str, Controller] = {}
+
+    def exists(self, name: str) -> bool:
+        """
+        Indicate whether the controller exists in the data service.
+        :param name: The name of the controller.
+        :return:
+        """
+        return name in self.__CONTROLLERS.keys()
+
+    def add(self, controller: Controller):
+        """
+        Add a controller in the service.
+        :param controller: The controller to add.
+        """
+        self.__CONTROLLERS[controller.name] = controller
+
+    def get(self, name: str) -> Controller:
+        """
+        Get a controller by name.
+        :param name: The name of the controller.
+        :return: The controller referenced by name.
+        """
+        return self.__CONTROLLERS.get(name)
